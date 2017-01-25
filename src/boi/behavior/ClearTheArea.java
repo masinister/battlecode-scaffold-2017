1c7f15fbb8771cd2eaf2a814578f9e826ae8ab9b
Froodly <douwze@yahoo.com>
2017-01-24 20:46:29 -0500

    Crazy better than good LUMBERJACK

--- src/boi/behavior/ClearTheArea.java
+++ src/boi/behavior/ClearTheArea.java
@@ -1,8 +1,6 @@
 package boi.behavior;
 
 import battlecode.common.*;
-import boi.behavior.move.BehaviorMove;
-import boi.behavior.move.TryMoveDirection;
 
 import java.util.Random;
 
@@ -11,57 +9,62 @@ import java.util.Random;
  */
 public class ClearTheArea extends Behavior {
 
-    private float dir = 0;
-    private double turnstep = 2.0;
-    private int turncount = 0;
+    private float dir = 0, initDir = 0;
+    private double toRads = 45.0;
+    private int treeI = 0;
     private boolean chopping = false;
     private TreeInfo[] trees;
-    private RobotInfo[] closeRobots, robots;
+    private RobotInfo[] closeRobots, allrobots;
     private Random rnd = new Random();
 
     public ClearTheArea(RobotController controller) {
         super(controller);
-        turnstep *= rnd.nextInt(3) + 1;
     }
     @Override
     public void step() throws GameActionException {
         chopping = false;
         trees = mController.senseNearbyTrees();
-        robots = mController.senseNearbyRobots(-1, mController.getTeam().opponent());
+        allrobots = mController.senseNearbyRobots(-1, mController.getTeam().opponent());
         closeRobots = mController.senseNearbyRobots(RobotType.LUMBERJACK.bodyRadius+GameConstants.LUMBERJACK_STRIKE_RADIUS - 1, mController.getTeam().opponent());
+        for (int i = 0; i < trees.length; i++) {
+            if (trees[i].getTeam() != mController.getTeam()) {
+                treeI = i;
+                break;
+            }
+        }
         if (trees.length > 0) {
-            if (mController.canChop(trees[0].getID()))
+            if (mController.canChop(trees[treeI].getID()))
                 chopping = true;
         }
         if (closeRobots.length > 0 && !mController.hasAttacked()) {
             if (mController.canStrike()) {
                 mController.strike();
-                turnstep /= 4.0;
             }
         }
         else if (!chopping) {
-            if (robots.length == 0) {
+            if (allrobots.length == 0) {
+                initDir = dir;
                 while (!mController.canMove(new Direction(dir))) {
-                    dir += Math.PI / turnstep;
-                    turncount++;
-                    if (turncount >= 1.0 * turnstep) {
-                        turncount = 0;
-                        turnstep *= -2.0;
-                    }
+                    dir += Math.PI / toRads;
+                    if (dir == initDir + (float) (2F * Math.PI))
+                        dir = initDir;
                 }
                 if (mController.canMove(new Direction(dir)))
                     mController.move(new Direction(dir));
-                dir -= Math.PI / (2.0 * turnstep);
+                dir += Math.PI / toRads;
+                if (dir >= 2.0 * Math.PI) {
+                    dir = 0;
+                    toRads += 30.0;
+                }
             } else {
-                if (mController.canMove(mController.getLocation().directionTo(robots[0].getLocation())))
-                    mController.move(mController.getLocation().directionTo(robots[0].getLocation()));
+                if (mController.canMove(mController.getLocation().directionTo(allrobots[0].getLocation())))
+                    mController.move(mController.getLocation().directionTo(allrobots[0].getLocation()));
             }
         } else {
-            if (mController.canChop(trees[0].getID()))
-                mController.chop(trees[0].getID());
+            if (mController.canChop(trees[treeI].getID()))
+                mController.chop(trees[treeI].getID());
             else chopping = false;
         }
-//        Clock.yield();
     }
 
     @Override
