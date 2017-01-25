493ce46fd022dd8490e7654c07951f0a6f89815e
masinister <mason.dicicco@gmail.com>
2017-01-25 14:08:24 -0500

    submitablE?

--- src/boi/behavior/masonGardener/GetAlone.java
+++ src/boi/behavior/masonGardener/GetAlone.java
@@ -22,21 +22,9 @@ public class GetAlone extends Behavior {
         RobotInfo robots[] = mController.senseNearbyRobots();
         if(robots.length>0) {
             MapLocation location = mController.getLocation().add(robots[0].getLocation().directionTo(mController.getLocation()),10);
-            for (RobotInfo robot : robots) {
-                if (robot.getType() == RobotType.LUMBERJACK) {
-                    location = robot.getLocation();
-                    break;
-                }
-            }
-            if(location!=null) {
-                Move move = new Move(mController, 30, 2, Move.to(location));
-                if (move.canStep())
-                    move.step();
-            } else {
-                Move move = new Move(mController, 30, 2, Move::randomly);
-                if (move.canStep())
-                    move.step();
-            }
+            Move move = new Move(mController, 30, 2, Move.to(location));
+            if (move.canStep())
+                move.step();
         }
         else {
             Move move = new Move(mController,45,2,Move::randomly);
